5841e9fedf24de7bac83708b0653991853e19503
Sumra Jabeen <160333925+Coderhubs@users.noreply.github.com>
2024-04-17 01:01:36 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+// declare a const "answers" and assign to the result of inquirer .prompt function
+
+const answers: {sentence : string} = await inquirer .prompt([
+    
+    { 
+        name: "sentence",
+        type : "input",
+        message: "Enter your sentence to count the word:"
+    }
+]);
+
+const words = answers.sentence.trim().split(" ")
+
+// print the array of words to the console
+console.log(words);
+
+// print the word count of the sentence to console
+console.log(`Your sentence word count is ${words.length}`);
+
